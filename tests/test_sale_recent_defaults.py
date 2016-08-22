09d422fd5a23b1abd562b4cc7a8163895b20f9d3
Raimon Esteve <raimon@nan-tic.com>
2016-08-22 09:54:19 +0200

    Tests

--- tests/test_sale_recent_defaults.py
+++ tests/test_sale_recent_defaults.py
@@ -1,33 +1,20 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
-# import doctest
+import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_recent_defaults')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_recent_defaults')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleRecentDefaultsTestCase(ModuleTestCase):
+    'Test Sale Recent Defaults module'
+    module = 'sale_recent_defaults'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_sale_recent_defaults.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        SaleRecentDefaultsTestCase))
     return suite
